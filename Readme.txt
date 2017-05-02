b13938af0e05e33542006faa59884b17e2af0fb8
Lee Abell <k2climber37@hotmail.com>
2017-05-02 21:39:56 +0100

    add files

---
+++ Readme.txt
@@ -0,0 +1,12 @@
+Calc distance between 2 postcodes
+=================================
+
+Handle client-server requests in Express using nodejs v7.9.0
+
+Download dependencies using : npm install
+
+Vist http://localhost:3000/ to view the app
+
+Distance between specified uk postcodes calculated via haversine formula will be displayed on the console.
+
+
---
+++ app.js
@@ -0,0 +1,84 @@
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//:::                                                                         :::
+//:::  This Application calculates the direct distance between two            :::
+//:::  specified postcodes.                                                   :::
+//:::                                                                         :::
+//:::                                                                         :::
+//:::                                                                         :::
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+var async = require('async');
+var PostcodesIO = require('postcodesio-client');
+var postcodes = new PostcodesIO();
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// serve landing page to browser
+app.get('/',function(req,res){
+  res.sendfile("index.html");
+});
+
+// handle calc request by landing page
+app.post('/calcdist',function(req,res){
+  var frompc=req.body.source;
+  var topc=req.body.dest;
+  console.log("POST request: From = "+frompc+" and To = "+topc);
+  async.waterfall(
+  [
+    function(done) {
+      var err = null;
+      var ll = [];
+      try {
+        coord(frompc).then(function(reslt){ll.push(reslt); done(err,ll);});
+      }
+      catch (e) {
+        console.log(e);
+      }
+      finally {return;}
+    },
+    function(ll,done) {
+      var err = null;
+      coord(topc).then(function(reslt){ll.push(reslt); done(err,ll);});
+    }], function(err, reslt) {var val=haversine(reslt);console.log("Distance (km) : "+val);});
+
+  res.end("yes");
+});
+
+// start express web server
+var server = app.listen(3000, function () {
+    console.log("Listening on port %s...", server.address().port);
+});
+
+// determine uk longitude and latitude from postcodes.io
+function coord(pcode) {
+  return new Promise((resolve, reject) => {
+    postcodes.lookup(pcode, function (err, postcode) {
+       if(err) reject(err); 
+       if(!postcode) return;
+       var LongLat = [postcode.longitude, postcode.latitude];
+       resolve (LongLat);
+    });
+  })
+}
+
+
+// use haversine formula to determine the great-circle distance between two points on a sphere given their longitudes and latitudes.
+// see https://en.wikipedia.org/wiki/Haversine_formula
+function haversine(res) {
+
+  Math.rads = function(degrees) {
+       return degrees * Math.PI / 180;
+  };
+
+  var R = 6372.8;
+  var dLat = Math.rads(res[1][1] - res[0][1]);
+  var dLon = Math.rads(res[1][0] - res[0][0]);
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(Math.rads(res[0][1])) * Math.cos(Math.rads(res[1][1])) * Math.sin(dLon/2) * Math.sin(dLon/2);
+  var c = 2 * Math.asin(Math.sqrt(a));
+  return R * c;
+}
+
